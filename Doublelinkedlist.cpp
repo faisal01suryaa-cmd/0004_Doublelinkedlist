15b554d59467b95b54ced8ebc9949ad9f916ce20
faisal01suryaa <faisal01suryaa@gmail.com>
2026-04-30 17:35:09 +0700

    langkah akhir untuk penghapusan

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -122,3 +122,10 @@ public:
             if (current->next != NULL)
                 current->next->prev = current->prev;
         }
+
+         // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
+    }
+
+
